3a4f828c323ba6c8ae15d8cb71e779ef70eaade5
MaurySnake <maury894@gmail.com>
2021-06-08 02:54:39 -0300

    fix

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.44"
+    }
+  }
+
+  required_version = ">= 0.15"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08962a4068733a2b6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
